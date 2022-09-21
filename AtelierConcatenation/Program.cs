b371ca5b8f4a73748ec2c5d91c3b86c41f5af0ed
RebeccaLapointe <RebeccaLapointe@INFO-386-2-P01>
2022-09-21 08:59:35 -0400

    Ajoutez des fichiers projet.

---
+++ AtelierConcatenation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AtelierConcatenation
+{
+    class AtelierConcatenation
+    {
+        static void Main(string[] args)
+        {
+            string nom = "Lachance";
+            string prenom = "Bob";
+            bool droitier = true;
+            string fruit = "l'ananas";
+            string condiment = "la moutarde";   // Cette chaîne de caractères pourrait être vide.
+            string ville = "Jonquière";
+            int nbAnneesVille = 8;       // Vous pouvez supposer que ce nombre est supérieur à 0.
+            int depart = 5;              // Vous pouvez supposer que ce nombre est supérieur à 0.
+
+            string phrase = "";
+
+            /* ... */
+
+            Console.WriteLine(phrase);
+        }
+    }
+}
